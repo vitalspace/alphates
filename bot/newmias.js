cbc1855c4deb4aaa828326dd8e8b4d4e4387e2e2
vitalspace <myglobalsounds@gmail.com>
2022-02-19 23:18:15 -0600

    Update newmias.js

--- bot/newmias.js
+++ bot/newmias.js
@@ -8,35 +8,45 @@ const { getExecutablePath } = require('../utils/utils')
 
 const program = async () => {
   const executablePath = await getExecutablePath({});
-  const webLists = {
-    web1: 'https://vitalspace.ml',
-    web2: 'https://smartwatches.gq',
-    web3: 'https://winetime.gq',
-    web4: 'https://hypercars.ml',
-    web5: 'https://catnation.cf',
-    web6: 'https://sneakersshop.gq',
-    web7: 'https://chameleons.ga',
-    web8: 'https://smartcells.gq',
-    web9: 'https://gamingzne.cf',
-    web10: 'https://roompool.gq',
-    web11: 'https://rushbanggeeks.ga',
-    web12: 'https://bagnation.ga',
-    web13: 'https://blakywhite.gq',
-    web14: 'https://codenerd.ml',
-    web15: 'https://cosplans.cf',
-    web16: 'https://darkcaps.ga',
-    web17: 'https://fastmotors.gq',
-    web18: 'https://judisnow.cf',
-    web19: 'https://milenianarqs.ml',
-    web20: 'https://wavesbag.cf',
-    web21: 'https://zuskim.ga',
-    web22: 'https://vestrig.ga',
-  };
+  const webLists = [
+    'https://vitalspace.ml',
+    'https://smartwatches.gq',
+    'https://winetime.gq',
+    'https://hypercars.ml',
+    'https://catnation.cf',
+    'https://sneakersshop.gq',
+    'https://chameleons.ga',
+    'https://smartcells.gq',
+    'https://gamingzne.cf',
+    'https://roompool.gq',
+    'https://rushbanggeeks.ga',
+    'https://bagnation.ga',
+    'https://blakywhite.gq',
+    'https://codenerd.ml',
+    'https://cosplans.cf',
+    'https://darkcaps.ga',
+    'https://fastmotors.gq',
+    'https://judisnow.cf',
+    'https://milenianarqs.ml',
+    'https://wavesbag.cf',
+    'https://zuskim.ga',
+    'https://vestrig.ga',
+  ];
 
-  Object.keys(webLists).forEach(async (k, i) => {
-    dir = path.join(__dirname, k);
-    await lauchpuppeteer({ executablePath }, webLists[k], dir);
-  });
+  let num = 0
+  let web = 'web'
+
+  for (const i of webLists) {
+    num ++
+    const newe = web + num
+  
+    // console.log(i, num, newe)
+      dir = path.join(__dirname, newe);
+      await lauchpuppeteer({ executablePath }, i, dir);
+  }
+
+  // Object.keys(webLists).forEach(async (k, i) => {
+  // });
 
 }
 
