337bb09d31088b599fcc98db274d1af6cf4331ae
vitalspace <myglobalsounds@gmail.com>
2022-02-20 23:36:33 -0600

    Update newmias.js

--- bot/newmias.js
+++ bot/newmias.js
@@ -1,67 +1,115 @@
 'use strict';
+const cron = require('node-cron');
 const puppeteer = require('puppeteer-core');
-const path = require('path');
 let chromePaths = require('chrome-paths');
-process.setMaxListeners(Infinity);
 
-(async () => {
-  const webLists = [
-    'https://vitalspace.ml',
-    'https://smartwatches.gq',
-    'https://winetime.gq',
-    'https://hypercars.ml',
-    'https://catnation.cf',
-    'https://sneakersshop.gq',
-    'https://chameleons.ga',
-    'https://smartcells.gq',
-    'https://gamingzne.cf',
-    'https://roompool.gq',
-    'https://rushbanggeeks.ga',
-    'https://bagnation.ga',
-    'https://blakywhite.gq',
-    'https://codenerd.ml',
-    'https://cosplans.cf',
-    'https://darkcaps.ga',
-    'https://fastmotors.gq',
-    'https://judisnow.cf',
-    'https://milenianarqs.ml',
-    'https://wavesbag.cf',
-    'https://zuskim.ga',
-    'https://vestrig.ga',
-  ];
+mylist()
 
-  let num = 0
-  let web = 'web'
-  let dir = {}
+cron.schedule('0 */25 * * * *', function () {
+  mylist()
+});
 
+async function mylist() {
   try {
-    for (const i of webLists) {
-      num++
-      const newe = web + num
-      dir = path.join(__dirname, newe);
+    const browser = await puppeteer.launch({
+      headless: true,
+      // userDataDir: dir,
+      executablePath: chromePaths.chrome,
+      args: [
+        '--incognito',
+        '--aggressive-cache-discard',
+        '--disable-audio-output',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-renderer-backgrounding',
+        '--disable-dev-shm-usage',
+        '--no-sandbox',
+        "--disable-setuid-sandbox"
+      ],
+      defaultViewport: null
+    });
 
-      const browser = await puppeteer.launch({
-        headless: false,
-        userDataDir: dir,
-        executablePath: chromePaths.chrome,
-        args: [
-          '--disable-audio-output',
-          '--disable-background-timer-throttling',
-          '--disable-backgrounding-occluded-windows',
-          '--disable-renderer-backgrounding',
-          '--disable-dev-shm-usage',
-          '--no-sandbox',
-          "--disable-setuid-sandbox"
-        ],
-        defaultViewport: null
-      });
+    browser.on('targetcreated', async (target) => { //
+      if (target.type() === 'page') {               // if it tab/page
+        const page = await target.page();      // declare it
+        const url = page.url();
+        if (url.search('https://vitalspace.ml') == -1) {     // if url is not like site.com (pop-up window whith ads to anower site)
 
-      const [page] = await browser.pages();
-      await page.goto(i, { waitUntil: 'networkidle0' });
+          setTimeout(async () => {
 
-    }
+            await page.close();           // close this page
+          }, 1320000);
+
+        }
+      }
+    });
+
+    const [page] = await browser.pages();
+    setTimeout(async () => {
+      await page.close();
+    }, 1320000);
+
+    await page.goto('https://vitalspace.ml', { timeout: 0, waitUntil: "networkidle2" });
+
+    await page.$eval('#web2', elem => elem.click());
+    await page.waitFor(5000)
+    await page.$eval('#web3', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web4', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web5', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web6', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web7', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web8', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web9', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web10', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web11', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web12', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web13', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web15', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web16', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web17', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web18', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web19', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web20', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web21', elem => elem.click());
+    await page.waitFor(5000)
+
+    await page.$eval('#web22', elem => elem.click());
+
   } catch (error) {
     console.log(error)
   }
-
-})();
+}
